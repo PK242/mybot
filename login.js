916e33d83e65176e5b0fcfcd62be680d13da9ab1
PK242 <38386197+PK242@users.noreply.github.com>
2018-08-02 22:31:12 -0500

    Update login.js

--- login.js
+++ login.js
@@ -11,11 +11,13 @@ var x;
 function myLogin() {
   x = document.getElementById("loginpass");
   if(x.value === "astro"){
-    
     window.open("/", "_self");
+    localStorage.login = 1;
   } else {
     if(x.value === ""){
+      localStorage.login = 0;
     } else {
+      localStorage.login = 0;
     }
   }
 }
